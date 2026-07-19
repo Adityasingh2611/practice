2321b264f1f1ce24b1b472d7efe8677a5454bc24
Adityasingh2611 <theadityasingh11@gmai.com>
2026-07-19 17:15:54 +0530

    Hi

---
+++ first.py
@@ -0,0 +1,17 @@
+#Finding modules and packages
+
+def m1():
+    print("first-m1")
+    return
+
+def m2():
+    print("first-m2")
+    return
+
+def m3():
+    print("first-m3")
+    return
+
+def m4():
+    print("Hello")
+    return
